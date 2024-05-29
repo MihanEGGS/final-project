2a9c080a3da8d83a44871738123405ac39b8508b
MihanEGGS <125635739+MihanEGGS@users.noreply.github.com>
2024-05-29 03:04:20 +0300

    Update tgbot_v2.py

--- tgbot_v2.py
+++ tgbot_v2.py
@@ -1,31 +1,37 @@
-from telegram import Update,InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler,Updater,  CallbackContext, BaseHandler
-import requests, random,json
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler, Updater, CallbackContext, BaseHandler
+import requests, random, json
+
 # izveido bota pieslēgumu Telegram
 bot_points = 0
 player_points = 0
-bots_choice = '1'    
+bots_choice = '1'
 player_previous_choice = None
 app = ApplicationBuilder().token("7112507583:AAGIDFfC-NsKjdeX_T1uj0_nOdvlps1oBas").build()
-# offers group members to join a game, after what person, who started the bot, chooses "Enough!"
+
+# Tit-for-tat strategy
 def tit_for_tat(player_choice, response):
     global bot_points, player_points, bots_choice, player_previous_choice, intervals, repetitions
-    bots_choice = '1' if player_previous_choice in [None, '1'] else '2'
-    if player_choice == bots_choice and player_choice == '1':
-        bot_points+= 3
+
+    bots_choice = '1' if player_previous_choice in [None, '1'] else '2'  # If player's previous choice is '1' or None, bot cooperates, else bot cheats
+
+    if player_choice == bots_choice and player_choice == '1':  # Both cooperate
+        bot_points += 3
         player_points += 3
-    elif player_choice != bots_choice and bots_choice == '1':
-        bot_points+= 0
+    elif player_choice != bots_choice and bots_choice == '1':  # Player cheats, bot cooperates
+        bot_points += 0
         player_points += 5
-    elif player_choice == bots_choice and player_choice == '2':
-        bot_points+= 1
+    elif player_choice == bots_choice and player_choice == '2':  # Both cheat
+        bot_points += 1
         player_points += 1
-    elif player_choice != bots_choice and bots_choice == '2':
-        bot_points+= 5
+    elif player_choice != bots_choice and bots_choice == '2':  # Player cooperates, bot cheats
+        bot_points += 5
         player_points += 0
-    response = "Your score is " + str(player_points) + '\n'+ "Bot's score is " + str(bot_points)
+
+    response = "Your score is " + str(player_points) + '\n' + "Bot's score is " + str(bot_points)
     player_previous_choice = player_choice
     return response
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global bot_points, player_points
     bot_points = 0
@@ -39,37 +45,40 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         intervals,
         is_anonymous=False,
         allows_multiple_answers=False,
-        open_period = -1
-        )
+        open_period=-1
+    )
+
 async def rules(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("RULEEEEEEEEEEEEZ"+'\n'+"You are playing with bot, and in your first move you can either cooperate or cheat your bot opponent"
-                                    +'\n'+"---If you and your opponent both choose to cooperate, you both gain 3 points"
-                                    + '\n'+ "---If one of you choose to cheat, that player, who cheats, gains 5 points and second playuer gains nothing "
-                                    + '\n'+ "--- If both players choose to cheat both players gain 1 point ")
+    await update.message.reply_text("RULEEEEEEEEEEEEZ" + '\n' + "You are playing with bot, and in your first move you can either cooperate or cheat your bot opponent"
+                                    + '\n' + "---If you and your opponent both choose to cooperate, you both gain 3 points"
+                                    + '\n' + "---If one of you choose to cheat, that player, who cheats, gains 5 points and second player gains nothing "
+                                    + '\n' + "--- If both players choose to cheat both players gain 1 point ")
+
 async def vote(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         global intervals, repetitions
         repetitions = 0
         completed_poll = await poll.stop_poll()
         print(completed_poll.to_dict())
-        for option in completed_poll.options:
+        for option in completed_poll.options:  # Loop through poll options
             print(option.text, option.voter_count)
-            if option.voter_count == 1:
-                await update.message.reply_text("The chosen interval is "+option.text +"We now start playing ")
-                if option.text == "8-15":
-                    intervals = random.randint(8,15) 
+            if option.voter_count == 1:  # If option was chosen
+                await update.message.reply_text("The chosen interval is " + option.text + " We now start playing ")
+                if option.text == "8-15":  # Set intervals based on choice
+                    intervals = random.randint(8, 15)
                 elif option.text == "18-25":
-                    intervals = random.randint(18,25)
+                    intervals = random.randint(18, 25)
                 elif option.text == "28-35":
-                    intervals = random.randint(28,35)
+                    intervals = random.randint(28, 35)
                 elif option.text == "38-45":
-                    intervals = random.randint(38,45)
+                    intervals = random.randint(38, 45)
         await game(update.effective_chat.id, context)
     except:
-        await update.message.reply_text ("Please, to start the game type /start")
+        await update.message.reply_text("Please, to start the game type /start")
+
 async def game(chat_id: int, context: ContextTypes.DEFAULT_TYPE):
     global repetitions, intervals
-    if repetitions < intervals:
+    if repetitions < intervals:  # Check if repetitions are less than intervals
         keyboard = [
             [
                 InlineKeyboardButton("Cooperate?", callback_data='1'),
@@ -84,9 +93,10 @@ async def game(chat_id: int, context: ContextTypes.DEFAULT_TYPE):
         )
     else:
         await context.bot.send_message(
-            chat_id=chat_id, 
+            chat_id=chat_id,
             text="Game over!"
         )
+
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global repetitions
     query = update.callback_query
@@ -98,11 +108,8 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
     repetitions += 1
     await game(query.message.chat_id, context)
 
-async def matches(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Type /matches to recieve tournament bracket")
 async def debil(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("I hear: Andrey is gay")
-
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("vote", vote))
